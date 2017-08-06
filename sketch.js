9c50166efb8e39fe14fc222f6f0b72b4bf83aa05
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2017-08-06 13:47:37 -0400

    Improve the color management

--- sketch.js
+++ sketch.js
@@ -42,8 +42,16 @@ function newText() {
             boxes.push([col, matches[i][w]]);
         }
 
-        var emptyColor = hexToRgb(palette[4]);
-        emptyColor = color(emptyColor.r, emptyColor.g, emptyColor.b);
+        // var emptyColor = hexToRgb(palette[4]);
+        var ac0 = hexToRgb(palette[0]);
+        var ac1 = hexToRgb(palette[1]);
+        var ac2 = hexToRgb(palette[2]);
+        var ac3 = hexToRgb(palette[3]);
+        var ac4 = hexToRgb(palette[4]);
+        var red = (ac0.r + ac1.r + ac2.r + ac3.r + ac4.r) / 5;
+        var green = (ac0.g + ac1.g + ac2.g + ac3.g + ac4.g) / 5;
+        var blue = (ac0.b + ac1.b + ac2.b + ac3.b + ac4.b) / 5;
+        var emptyColor = color(red, green, blue);
 
         boxes.push([emptyColor, null]);
         // fill(col);
@@ -73,7 +81,8 @@ function fillSheet() {
                 rect(0, 0, columnWidth, columnWidth);
                 if (boxes[num][1]) {
                     translate(columnWidth / 4, -columnWidth / 4);
-                    fill(0);
+                    var opp = getOppositeColor(boxes[num][0]);
+                    fill(red(opp), green(opp), blue(opp), 190);
                     text(boxes[num][1], 0, 0 + columnWidth);
                 }
                 pop();
@@ -89,6 +98,18 @@ function selectColor(POS) {
         case "nn":
             col = [0];
             break;
+        case "nns":
+            col = [0];
+            break;
+        case "dt":
+            col = [0, 1];
+            break;
+        case "prp":
+            col = [0, 2];
+            break;
+        case "prp$":
+            col = [0, 2];
+            break;
         case "nnp":
             col = [0, 4];
             break;
@@ -96,11 +117,16 @@ function selectColor(POS) {
             col = [1];
             break;
         case "jjr":
-            col = [1, 0];
+            col = [1];
             break;
+        case "jjs":
+            col = [1];
+            break;
+
         case "rb":
-            col = [2];
+            col = [4];
             break;
+
         case "vb":
             col = [3];
             break;
@@ -111,14 +137,15 @@ function selectColor(POS) {
             col = [3, 1];
             break;
         case "vbp":
-            col = [4, 0];
-            break;
-        case "vbz":
             col = [3, 2];
             break;
-        case "vbn":
+        case "vbz":
             col = [3, 4];
             break;
+        case "vbn":
+            col = [2, 4];
+            break;
+
         default:
             col = [3, 4];
     }
@@ -141,4 +168,44 @@ function interpretColor(col) {
         pal = color((color1.r + color2.r) / 2, (color1.g + color2.g) / 2, (color1.b + color2.b) / 2);
     }
     return pal;
+}
+
+function keyPressed() {
+    if (key == 'p' || key == 'P') {
+        currentPaletteIndex = Math.floor(Math.random() * 4000);
+        newText();
+        fillSheet();
+    }
+}
+
+function getOppositeColor(col) {
+    var currentSum = red(col) + green(col) + blue(col);
+    var ac0 = hexToRgb(palette[0]);
+    var ac1 = hexToRgb(palette[1]);
+    var ac2 = hexToRgb(palette[2]);
+    var ac3 = hexToRgb(palette[3]);
+    var ac4 = hexToRgb(palette[4]);
+    var sum0 = ac0.r + ac0.g + ac0.b;
+    var sum1 = ac1.r + ac1.g + ac1.b;
+    var sum2 = ac2.r + ac2.g + ac2.b;
+    var sum3 = ac3.r + ac3.g + ac3.b;
+    var sum4 = ac4.r + ac4.g + ac4.b;
+    var sums = [
+        [sum0, ac0],
+        [sum1, ac1],
+        [sum2, ac2],
+        [sum3, ac3],
+        [sum4, ac4]
+    ];
+    var difference = 0;
+    var finalCol;
+    for (var i = 0; i < sums.length; i++) {
+        var currentDifference = Math.abs(currentSum - sums[i][0]);
+        // console.log(currentDifference);
+        if (currentDifference > difference) {
+            difference = currentDifference;
+            finalCol = sums[i][1];
+        }
+    }
+    return color(finalCol.r, finalCol.g, finalCol.b);
 }
